976601fafda507cebfe982e43fb4a330d1f4c042
NlodeL <nlodelop@myuax.com>
2024-02-07 21:47:54 +0100

    finalmente importo todas las clases necesarias

--- ejercicio7.java
+++ ejercicio7.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class ejercicio7 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
