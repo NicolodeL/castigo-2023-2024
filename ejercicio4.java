64cbe39a90e7aafdefde8d69722f763dadd0acfc
NlodeL <nlodelop@myuax.com>
2024-02-07 20:43:05 +0100

    añado instrucciones

--- ejercicio4.java
+++ ejercicio4.java
@@ -5,5 +5,7 @@ public class ejercicio4 {
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
 
+        System.out.println("Introduce números enteros positivos (introduce un número negativo para finalizar):");
+
     }
 }
