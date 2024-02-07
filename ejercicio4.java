deefc80af9d018d642e973bacdc1e4af484defaa
NlodeL <nlodelop@myuax.com>
2024-02-07 20:42:17 +0100

    agrego una funcion que definiré mas tarde y datos de minimos y maximos

--- ejercicio4.java
+++ ejercicio4.java
@@ -1,2 +1,9 @@
 public class ejercicio4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+    }
 }
