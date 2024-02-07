92261a9c152acbe80a3961c1ea4a9299386432c8
NlodeL <nlodelop@myuax.com>
2024-02-07 21:50:57 +0100

    creo el scanner y de ahí defino el nuevo método para que me proporcione un resultado

--- ejercicio8.java
+++ ejercicio8.java
@@ -9,4 +9,34 @@ public class ejercicio8 {
         System.out.println("Mínimo: " + minimo);
         System.out.println("Máximo: " + maximo);
     }
+    public static int[] recibirNumeros() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese números enteros (0 para terminar):");
+
+        int capacidadInicial = 10;
+        int[] numeros = new int[capacidadInicial];
+        int contador = 0;
+
+        while (true) {
+            int numero = scanner.nextInt();
+
+            if (numero == 0) {
+                break;
+            }
+
+            if (contador == numeros.length) {
+                numeros = aumentarCapacidad(numeros);
+            }
+
+            numeros[contador] = numero;
+            contador++;
+        }
+
+        scanner.close();
+
+        int[] resultado = new int[contador];
+        System.arraycopy(numeros, 0, resultado, 0, contador);
+
+        return resultado;
+    }
 }
