69d7193ecee0e2585f3196b37f5391ee79354148
NlodeL <nlodelop@myuax.com>
2024-02-07 21:39:48 +0100

    creo el primer método para obtener números primos

--- ejercicio6.java
+++ ejercicio6.java
@@ -1,3 +1,8 @@
 import java.util.Arrays;
 public class ejercicio6 {
+    public static void main(String[] args) {
+        int N = 10; // Número de primeros primos a mostrar
+        int[] primos = obtenerPrimerosPrimos(N);
+        System.out.println(Arrays.toString(primos));
+    }
 }
