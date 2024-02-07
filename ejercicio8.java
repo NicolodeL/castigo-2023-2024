2433453d1c7f0bba6746a2e56ec625c3c9a42b60
NlodeL <nlodelop@myuax.com>
2024-02-07 21:49:44 +0100

    añado los primeros datos que me harán falta

--- ejercicio8.java
+++ ejercicio8.java
@@ -1,2 +1,12 @@
 public class ejercicio8 {
+    public static void main(String[] args) {
+        int[] numeros = recibirNumeros();
+        double media = calcularMedia(numeros);
+        int minimo = calcularMinimo(numeros);
+        int maximo = calcularMaximo(numeros);
+
+        System.out.println("Media: " + media);
+        System.out.println("Mínimo: " + minimo);
+        System.out.println("Máximo: " + maximo);
+    }
 }
