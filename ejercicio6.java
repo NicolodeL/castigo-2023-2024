88d9cfd328351ca7fb1e55581b12ae3645c8e28e
NlodeL <nlodelop@myuax.com>
2024-02-07 21:41:53 +0100

    finalmente añado el codigo que pide el ejercicio para que de el resultado esperado

--- ejercicio6.java
+++ ejercicio6.java
@@ -20,4 +20,17 @@ public class ejercicio6 {
 
         return primos;
     }
+    public static boolean esPrimo(int numero) {
+        if (numero <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(numero); i++) {
+            if (numero % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
