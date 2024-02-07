7939bb7b8b6049b159c058bc9be1bea5096eef08
NlodeL <nlodelop@myuax.com>
2024-02-07 21:35:10 +0100

    arreglo el ejercicio 2 para que compile también

--- ejercicio2.java
+++ ejercicio2.java
@@ -1,5 +1,8 @@
 public class ejercicio2 {
-    public static <String> void mayor(String[] args) {
+    public static void main(String[] args) {
+        mayor();
+    }
+    public static void mayor() {
         char mayorChar = (char) (Math.pow(2, 16) - 1);
         short mayorShort = (short) (Math.pow(2, 15) - 1);
         int mayorInt = (int) (Math.pow(2, 31) - 1);
