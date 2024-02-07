17c217a7c026d98e9b0ec9114080350cdc368a42
NlodeL <nlodelop@myuax.com>
2024-02-07 20:36:29 +0100

    creo dos funciones

--- ejercicio3.java
+++ ejercicio3.java
@@ -2,6 +2,10 @@ public class ejercicio3 {
     public static void main(String[] args) {
         int begin = 1;
         int end = 100;
+
+        int[] multiplosDe5 = obtenerMultiplosDe5(begin, end);
+        int sumaMultiplosDe5 = sumarValoresArray(multiplosDe5);
+
         
     }
 }
