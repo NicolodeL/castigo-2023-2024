305e4d112d740acb187d6f66a941cb52ae089846
NlodeL <nlodelop@myuax.com>
2024-02-07 20:23:57 +0100

    pongo disitntos valores para un char,un short y un int.

--- ejercicio2.java
+++ ejercicio2.java
@@ -1,5 +1,8 @@
 public class ejercicio2 {
     public static <String> void mayor(String[] args) {
-        
+        char mayorChar = (char) (Math.pow(2, 16) - 1);
+        short mayorShort = (short) (Math.pow(2, 15) - 1);
+        int mayorInt = (int) (Math.pow(2, 31) - 1);
+
 }
 }
