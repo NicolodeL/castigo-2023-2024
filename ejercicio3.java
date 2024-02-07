224d78370844fbbdd6a87cc511dc68d7016fcfef
NlodeL <nlodelop@myuax.com>
2024-02-07 20:37:21 +0100

    pongo el codigo para mostrar los metodos a la hora de compilar

--- ejercicio3.java
+++ ejercicio3.java
@@ -6,6 +6,13 @@ public class ejercicio3 {
         int[] multiplosDe5 = obtenerMultiplosDe5(begin, end);
         int sumaMultiplosDe5 = sumarValoresArray(multiplosDe5);
 
-        
+        System.out.println("Los múltiplos de 5 comprendidos entre " + begin + " y " + end + " son:");
+        for (int multiplo : multiplosDe5) {
+            System.out.println(multiplo);
+        }
+
+        System.out.println("La suma de los múltiplos de 5 es: " + sumaMultiplosDe5);
+        System.out.println("La cantidad de múltiplos de 5 es: " + multiplosDe5.length);
+    }
     }
 }
