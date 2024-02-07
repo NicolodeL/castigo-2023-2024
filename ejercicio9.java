897c39af2f6cf9df69c52f6487db56d42e616967
NlodeL <nlodelop@myuax.com>
2024-02-07 21:55:59 +0100

    creo el scanner y  disintintos datos

--- ejercicio9.java
+++ ejercicio9.java
@@ -1,2 +1,16 @@
 public class ejercicio9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese la dimensión de los vectores: ");
+        int dimension = scanner.nextInt();
+
+        int[] vector1 = leerVector(dimension);
+        int[] vector2 = leerVector(dimension);
+
+        int productoEscalar = calcularProductoEscalar(vector1, vector2);
+
+        System.out.println("El producto escalar de los vectores es: " + productoEscalar);
+    }
+
 }
