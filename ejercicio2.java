91d5628d038241b6a16085db606c25c24130ac22
NlodeL <nlodelop@myuax.com>
2024-02-07 20:24:23 +0100

    añado el codigo faltante para que se vea al compilarlo

--- ejercicio2.java
+++ ejercicio2.java
@@ -4,5 +4,9 @@ public class ejercicio2 {
         short mayorShort = (short) (Math.pow(2, 15) - 1);
         int mayorInt = (int) (Math.pow(2, 31) - 1);
 
+        System.out.println("El mayor número entero representable en un char es: " + (int) mayorChar);
+        System.out.println("El mayor número entero representable en un short es: " + mayorShort);
+        System.out.println("El mayor número entero representable en un int es: " + mayorInt);
+
 }
 }
