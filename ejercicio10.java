42717cebad62796ab06eefe310555e295ca5296e
NlodeL <nlodelop@myuax.com>
2024-02-07 22:02:34 +0100

    finalmente hago el ultimo método para que devuelva la matriz generada

--- ejercicio10.java
+++ ejercicio10.java
@@ -23,4 +23,12 @@ public class ejercicio10 {
 
         return matriz;
     }
+    public static void imprimirMatriz(int[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
