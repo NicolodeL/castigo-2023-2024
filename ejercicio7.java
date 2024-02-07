521cc3029ef8c636827ce639029f1d317d96dabb
NlodeL <nlodelop@myuax.com>
2024-02-07 21:45:37 +0100

    creo un Scanner y un método para ingresar un número entero

--- ejercicio7.java
+++ ejercicio7.java
@@ -1,2 +1,18 @@
 public class ejercicio7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese un número entero: ");
+        int numero = scanner.nextInt();
+        scanner.close();
+
+        int[] factoresPrimos = obtenerFactoresPrimos(numero);
+        System.out.print(numero + " = ");
+        for (int i = 0; i < factoresPrimos.length; i++) {
+            System.out.print(factoresPrimos[i]);
+            if (i < factoresPrimos.length - 1) {
+                System.out.print(" * ");
+            }
+        }
+    }
+    
 }
