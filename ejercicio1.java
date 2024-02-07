7982520fc2438ff74eac3e65752e103619a17b12
NlodeL <nlodelop@myuax.com>
2024-02-07 20:14:08 +0100

    añado los valores a la clase tiempo

--- ejercicio1.java
+++ ejercicio1.java
@@ -1,6 +1,9 @@
 public class ejercicio1 {
     public static <String> void tiempo(String[] args) {
+        int diasEnUnAnio = 365;
+        byte horasEnUnDia = 24;
+        byte minutosEnUnaHora = 60;
+        byte segundosEnUnMinuto = 60;
 
     }
     }
-}
