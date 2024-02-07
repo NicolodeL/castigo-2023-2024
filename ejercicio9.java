cf551a2f9dc88c363a82cb6cf61ae88ada29496d
NlodeL <nlodelop@myuax.com>
2024-02-07 21:57:21 +0100

    añado el siguiente método

--- ejercicio9.java
+++ ejercicio9.java
@@ -12,5 +12,17 @@ public class ejercicio9 {
 
         System.out.println("El producto escalar de los vectores es: " + productoEscalar);
     }
+    public static int[] leerVector(int dimension) {
+        Scanner scanner = new Scanner(System.in);
+        int[] vector = new int[dimension];
+
+        System.out.println("Ingrese los elementos del vector separados por espacios:");
+
+        for (int i = 0; i < dimension; i++) {
+            vector[i] = scanner.nextInt();
+        }
+
+        return vector;
+    }
 
 }
